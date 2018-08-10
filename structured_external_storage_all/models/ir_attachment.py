4661ca99d6eb1596080f2ed016b670b1ca4a9b2c
kiplangatdan <kiplangatdan@gmail.com>
2018-08-10 18:43:15 +0300

    [FIX]sycn rule sequence

--- structured_external_storage_all/models/ir_attachment.py
+++ structured_external_storage_all/models/ir_attachment.py
@@ -17,36 +17,36 @@ class Attachment(models.Model):
     def create_metadata(self):
         """Use sequence to obtain the rule with high priority"""
         sync_rule_obj = self.env['ir.attachment.sync.rule']
-        all_rules = sync_rule_obj.search([]).mapped(
-            'source_model.object')
         for attachment in self:
             # check if res_model is in the define rules.
-            if attachment.res_model in all_rules:
-                sync_rule = sync_rule_obj.search(
-                    [('source_model.object', '=', attachment.res_model),
-                     ('sequence', '<', 10)],
-                    order='sequence asc')
-                if not sync_rule:
-                    sync_rule = sync_rule_obj.search(
-                        [('source_model.object', '=', attachment.res_model)],
-                        order='sequence asc', limit=1)
-
-                for rule in sync_rule:
-                    # Check if attachment already exists in the metas
-                    metadata_obj = self.env['ir.attachment.metadata']
-                    existing_metas = metadata_obj.search(
-                        [('attachment_id', '=', attachment.id)])
-                    locations = existing_metas.mapped('location_id').ids
-                    if not existing_metas or rule.location_id.id not in \
-                            locations:
-                        # Gather vals from sync_rule
-                        vals = {
-                            'attachment_id': attachment.id,
-                            'name': attachment.datas_fname[:-4],
-                            'type': 'binary',
-                            'task_id': rule.location_id.task_ids[0].id,  # get the first
-                            'file_type': 'export_external_location',
-                        }
-                        metadata_obj.create(vals)
+            all_matching_rules = sync_rule_obj.search([
+                ('source_model.object', '=', attachment.res_model)
+            ], order='sequence asc')
+            selected_matching_rules = []
+            for rule in all_matching_rules:
+                previous_rules_with_same_loc_and_lower_seq = [
+                    previous_rule for previous_rule in selected_matching_rules
+                    if previous_rule.location_id == rule.location_id
+                       and previous_rule.sequence < rule.sequence
+                ]
+                if not previous_rules_with_same_loc_and_lower_seq:
+                    selected_matching_rules.append(rule)
+            for rule in selected_matching_rules:
+                # Check if attachment already exists in the metas
+                metadata_obj = self.env['ir.attachment.metadata']
+                existing_metas = metadata_obj.search(
+                    [('attachment_id', '=', attachment.id)])
+                locations = existing_metas.mapped('location_id').ids
+                if not existing_metas or rule.location_id.id not in \
+                        locations:
+                    # Gather vals from sync_rule
+                    vals = {
+                        'attachment_id': attachment.id,
+                        'name': attachment.datas_fname[:-4],
+                        'type': 'binary',
+                        'task_id': rule.location_id.task_ids[0].id,  # get the first
+                        'file_type': 'export_external_location',
+                    }
+                    metadata_obj.create(vals)
         return True
 
